4a2bbebddea8c3f46baf3f48388691dba49c52eb
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-08-05 15:30:07 +0430

    client messages create
    all dangofang of client messages are created(route, migration, controller, mode.... )

---
+++ app/Models/ClientsMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientsMessage extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClientsMessageFactory> */
+    use HasFactory;
+}
