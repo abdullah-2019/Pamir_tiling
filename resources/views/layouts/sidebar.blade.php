efaacee54e7bbd5fadeb5da55d0565197d9684c7
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-08-23 15:36:25 +0430

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -151,35 +151,6 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon bi bi-tree-fill"></i>
-                        <p>
-                            UI Elements
-                            <i class="nav-arrow bi bi-chevron-right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="./UI/general.html" class="nav-link">
-                                <i class="nav-icon bi bi-circle"></i>
-                                <p>General</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./UI/icons.html" class="nav-link">
-                                <i class="nav-icon bi bi-circle"></i>
-                                <p>Icons</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./UI/timeline.html" class="nav-link">
-                                <i class="nav-icon bi bi-circle"></i>
-                                <p>Timeline</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
                         <i class="nav-icon bi bi-pencil-square"></i>
                         <p>
                             Forms
