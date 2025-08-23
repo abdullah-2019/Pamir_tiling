cc81c3e6e52cb9b6a93876959d1cb4622cdfb1e4
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-08-23 15:37:32 +0430

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -206,29 +206,6 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon bi bi-ui-checks-grid"></i>
-                        <p>
-                            Components
-                            <i class="nav-arrow bi bi-chevron-right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="./docs/components/main-header.html" class="nav-link">
-                                <i class="nav-icon bi bi-circle"></i>
-                                <p>Main Header</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./docs/components/main-sidebar.html" class="nav-link">
-                                <i class="nav-icon bi bi-circle"></i>
-                                <p>Main Sidebar</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
                         <i class="nav-icon bi bi-filetype-js"></i>
                         <p>
                             Javascript
