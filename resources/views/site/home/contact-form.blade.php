8587e5e554a703636f833806d5a92f6bf5766467
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-08-26 15:43:09 +0430

    Update contact-form.blade.php

--- resources/views/site/home/contact-form.blade.php
+++ resources/views/site/home/contact-form.blade.php
@@ -4,37 +4,37 @@
 
         <div class="search-field">
             <label for="first-name" class="field-label">First Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your first name" required>
+            <input type="text" id="name" name="name" placeholder="Enter your first name" required max="255">
             <i class="bi bi-person field-icon"></i>
         </div>
 
         <div class="search-field">
             <label for="last-name" class="field-label">Last Name</label>
-            <input type="text" id="last_name" name="last_name" placeholder="Enter your last name" required>
+            <input type="text" id="last_name" name="last_name" placeholder="Enter your last name" required max="255">
             <i class="bi bi-person field-icon"></i>
         </div>
 
         <div class="search-field">
             <label for="email" class="field-label">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required>
+            <input type="email" id="email" name="email" placeholder="Enter your email" required max="255">
             <i class="bi bi-envelope field-icon"></i>
         </div>
 
         <div class="search-field">
             <label for="phone" class="field-label">Contact Number</label>
-            <input type="text" id="phone" name="phone" placeholder="Enter your phone" required>
+            <input type="text" id="phone" name="phone" placeholder="Enter your phone" required max="255">
             <i class="bi bi-telephone field-icon"></i>
         </div>
 
         <div class="search-field">
             <label for="subject" class="field-label">Subject</label>
-            <input type="text" id="subject" name="subject" placeholder="Enter the subject" required>
+            <input type="text" id="subject" name="subject" placeholder="Enter the subject" required max="255">
             <i class="bi bi-chat-left-text field-icon"></i>
         </div>
 
         <div class="search-field">
             <label for="body" class="field-label">How can we help?</label>
-            <textarea id="help" name="message" rows="4" placeholder="How we can help you?" required></textarea>
+            <textarea id="help" name="message" rows="4" placeholder="How we can help you?" required maxlength="65500"></textarea>
             <i class="bi bi-question-circle field-icon"></i>
         </div>
 
