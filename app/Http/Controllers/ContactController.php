9cf6901bcfb54cbc897988f1af8b4e72b934e30d
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-08-29 18:34:51 +0430

    bugs fixed

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except(['page']);
+        $this->middleware('auth')->except(['page', 'store']);
     }
 
     /**
--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -12,7 +12,7 @@ class ProjectsController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('auth')->except(['page']);
+        $this->middleware('auth')->except(['page', 'projectDetial']);
     }
     /**
      * Display a listing of the resource.
